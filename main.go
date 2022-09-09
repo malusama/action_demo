6f459735a4dc41623c719d2b1ec7f6691a0272e3
malusama <chensichengmalu@gmail.com>
2022-09-09 16:06:16 +0800

    add build ci

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+func add(x, y int) int {
+	return x + y
+}
+
 func main() {
 	fmt.Println("hello world")
 }
---
+++ main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type args struct {
+	x int
+	y int
+}
+
+func TestAdd(t *testing.T) {
+	tests := []struct {
+		name   string
+		args   args
+		result int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "add 1 and 2",
+			args: args{
+				x: 1,
+				y: 2,
+			},
+			result: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.result, add(tt.args.x, tt.args.y))
+		})
+	}
+}
