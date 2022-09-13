0f75cfd1dd03f552c7190f97ef84fdd0096978b0
malusama <chensichengmalu@gmail.com>
2022-09-13 15:38:56 +0800

    update

--- main_test.go
+++ main_test.go
@@ -12,6 +12,7 @@ type args struct {
 }
 
 func TestAdd(t *testing.T) {
+	t.Parallel()
 	tests := []struct {
 		name   string
 		args   args
@@ -29,6 +30,7 @@ func TestAdd(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			assert.Equal(t, tt.result, Add(tt.args.x, tt.args.y))
 		})
 	}
